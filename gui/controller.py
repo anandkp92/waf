e959f11687370e2badefcc2da7bf5e7391c2c99c
Anand Krishnan <anandkp92@gmail.com>
2015-06-18 05:54:49 +0530

    added tabs and in-code documentation

--- gui/controller.py
+++ gui/controller.py
@@ -3,22 +3,28 @@ import os
 import view
 
 class Controller:
-	def __init__(self, app):
-		self.view = view.View(None, 'RTEMS Config')
-		self.view.Bind(wx.EVT_MENU, self.quit_event, self.view.qmi)
-		self.view.Bind(wx.EVT_MENU, self.new_event, self.view.new_cfg)
-		self.view.Bind(wx.EVT_MENU, self.open_event, self.view.open_cfg)
-		self.view.Bind(wx.EVT_MENU, self.save_event, self.view.save_cfg)
+	def __init__(self, app, no_of_tabs):
+		##== create an object of the View module ==##
+		self.view = view.View(None, 'RTEMS Config', no_of_tabs)
+		
+		##== set up event handlers for the menu items ==##
+		self.view.mb.Bind(wx.EVT_MENU, self.quit_event, self.view.mb.qmi)
+		self.view.mb.Bind(wx.EVT_MENU, self.new_event, self.view.mb.new_cfg)
+		self.view.mb.Bind(wx.EVT_MENU, self.open_event, self.view.mb.open_cfg)
+		self.view.mb.Bind(wx.EVT_MENU, self.save_event, self.view.mb.save_cfg)
 		self.view.Show(True)
 
+	##== event handler when the user chooses Quit menu item ==##
 	def quit_event(self, e):
 		self.view.Close(True)
-	
+
+	##== event handler when the user chooses New menu item ==##	
 	def new_event(self, e):
 		dlg = wx.MessageDialog(self.view, "Do you want to exit without Saving?", "Confirmation")
 		dlg.ShowModal()
 		dlg.Destroy()
 
+	##== event handler when the user chooses Open menu item ==##
 	def open_event(self,e):
         	self.dirname = ''
 		dlg = wx.FileDialog(self.view, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
@@ -30,6 +36,7 @@ class Controller:
 			f.close()
 			dlg.Destroy()
 
+	##== event handler when the user chooses Save menu item ==##
         def save_event(self,e):
                 self.dirname = ''
                 dlg = wx.FileDialog(self.view, "Choose a file", self.dirname, "", "*.*", wx.SAVE)
@@ -42,6 +49,7 @@ class Controller:
                         dlg.Destroy()
 	
 if __name__ == '__main__':
+	no_of_tabs = 3
 	app = wx.App(False)
-	controller = Controller(app)
+	controller = Controller(app, no_of_tabs)
 	app.MainLoop()
--- gui/view.py
+++ gui/view.py
@@ -1,9 +1,19 @@
 import wx
 class View(wx.Frame):
-        def __init__(self, parent, title):
+	def __init__(self, parent, title, no_of_tabs):
                 wx.Frame.__init__(self, parent, title = title)
+		##== tab layout ==##
+		self.nb = tabSet(self, no_of_tabs)
+		##== setting up menus ==##
+		self.mb  = menubar()
+		self.SetMenuBar(self.mb)
 
-                fileMenu = wx.Menu()
+class menubar(wx.MenuBar):
+	def __init__(self):
+		wx.MenuBar.__init__(self)
+
+		##== set up File, View and Tools menu  ==##
+		fileMenu = wx.Menu()
                 self.new_cfg = fileMenu.Append(wx.ID_NEW, '&New')
                 self.open_cfg = fileMenu.Append(wx.ID_OPEN, '&Open')
                 self.save_cfg = fileMenu.Append(wx.ID_SAVE, '&Save')
@@ -20,10 +30,22 @@ class View(wx.Frame):
                 toolsMenu.Append(wx.ID_ANY, '&Build')
                 toolsMenu.AppendSeparator()
 
-                menubar = wx.MenuBar()
-                menubar.Append(fileMenu, '&File')
-                menubar.Append(viewMenu, '&View')
-                menubar.Append(toolsMenu, '&Tools')
-                self.SetMenuBar(menubar)
-                #self.Show(True)
+                self.Append(fileMenu, '&File')
+                self.Append(viewMenu, '&View')
+                self.Append(toolsMenu, '&Tools')
 
+class tabSet(wx.Notebook):
+	##== set up a list of tabs/panes ==##
+	def __init__(self, parent, no_of_tabs):
+                nb = wx.Notebook(parent)
+                self.tabs = []
+                for i in range(0,no_of_tabs):
+                        name = "Tab "+str(i+1)
+                        self.tabs.append(tabPanel(nb, name))
+                        nb.AddPage(self.tabs[i], name)
+                self.tabs[0].SetFocus()	
+
+class tabPanel(wx.Panel):
+	##== each tab is a separate Panel ==##
+        def __init__(self, parent, name):
+                wx.Panel.__init__(self, parent, name = name)
