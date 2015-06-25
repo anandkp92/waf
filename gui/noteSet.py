2b0303f2a5f8f084cede0de673f2193fb71006a8
Anand Krishnan <anandkp92@gmail.com>
2015-06-25 17:48:19 +0530

    added GBoolean class. TODO: event handlers

--- gui/noteSet.py
+++ gui/noteSet.py
@@ -33,11 +33,14 @@ class createPanels:
 				v = opt.value
 			else:
 				v = "must set value"
-			p = BasePanel(self.base, n, d, v)
+			if opt.__base__.__name__ == 'Boolean':
+				p = GBoolean(self.base, n, d, v)
+			else:
+				p = BasePanel(self.base, n, d, v)
 			self.panels.append(p)
 		box = wx.BoxSizer(wx.VERTICAL)
 		for p in self.panels:
-		        box.Add(p, wx.EXPAND)
+		        box.Add(p)#, wx.EXPAND)
 
 		self.base.SetSizer(box)
 		return self.base
@@ -45,14 +48,50 @@ class createPanels:
 class BasePanel(wx.Panel):
 	def __init__(self, parent, name, desc, value):
 		wx.Panel.__init__(self, parent=parent, id = wx.ID_ANY)
-		item1 = wx.StaticText(self, -1, label = name)
-		item2 = wx.StaticText(self, -1, label = "Default : %s"%(str('True')))
-		item3 = wx.StaticText(self, -1, label = "Desc : %s"%desc)
-		item4 = wx.StaticText(self, -1, label = "Value : %s"%value)
-		grid = wx.GridSizer(5, 1)
-		grid.Add(item1, wx.EXPAND)
-		grid.Add(item2, wx.EXPAND)
-		grid.Add(item3, wx.EXPAND)
-		grid.Add(item4, wx.EXPAND)
-		self.SetSizer(grid)
+		self.value = value
+		self.item1 = wx.StaticText(self, -1, label = name)
+		self.item2 = wx.StaticText(self, -1, label = "About : %s"%desc)
+		self.item3 = wx.StaticText(self, -1, label = "Default Value : %s"%self.value)
+		self.item4 = wx.Button(self, -1, "Reset")
 
+		#self.item4.Disable()
+		self.grid = wx.GridSizer(5, 1)
+
+		self.grid.Add(self.item1, 1, wx.EXPAND)
+		self.grid.Add(self.item2, 1, wx.EXPAND)
+		self.grid.Add(self.item3, 1,wx.EXPAND)
+		self.grid.Add(self.item4, wx.EXPAND)
+		self.SetSizer(self.grid)
+
+class GBoolean(BasePanel):
+	def __init__(self, parent, name, desc, value):
+		BasePanel.__init__(self, parent, name, desc, value)
+		#self.value = value
+		self.smallPanel = wx.Panel(self)
+		self.grid2 = wx.GridSizer(1,2)
+		self.rbTrue = wx.RadioButton(self.smallPanel, id=wx.ID_ANY, label = "True", style=wx.RB_GROUP)
+		self.rbFalse = wx.RadioButton(self.smallPanel, id=wx.ID_ANY, label = "False")
+
+		#TODO: put in controller class
+		'''
+		self.rbTrue.Bind(wx.EVT_RADIOBUTTON, self.radio_event, self.rbTrue)
+		self.rbFalse.Bind(wx.EVT_RADIOBUTTON, self.radio_event, self.rbFalse)
+		
+		if self.value == 'True':
+			self.rbTrue.SetValue(True)
+		else:
+			self.rbFalse.SetValue(True)
+		'''
+		self.grid2.Add(self.rbTrue, wx.EXPAND)
+		self.grid2.Add(self.rbFalse, wx.EXPAND)
+		self.smallPanel.SetSizer(self.grid2)
+		
+		self.grid.Add(self.smallPanel, wx.EXPAND)
+		self.SetSizer(self.grid)
+	'''
+	def radio_event(self, event):
+		if self.rbTrue == 'True' and self.value != 'True':
+			self.item4.Enable()
+		elif self.rbFalse == 'True' and self.value != 'False':
+			self.item4.Enable()
+	'''
