380cabeffc17eb67ccb4632ae83354df885df1cd
Anand Krishnan <anandkp92@gmail.com>
2015-07-22 04:43:55 +0530

    function to get list of all bsps

---
+++ gui/getBSP.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# encoding: UTF-8
+
+'''This file defines functions to get all bsps defined in py/waf/defaults/bsp'''
+
+import wx
+import inspect
+
+def get_bsp_classes(module):
+        bsp_list = []
+        skip = ("__class__", "Base", "Config")
+
+        for tmp_name, tmp_obj in inspect.getmembers(module):
+                for name, obj in inspect.getmembers(tmp_obj):
+                        if inspect.isclass(obj) and name not in skip:
+                                bsp_list.append(obj)
+        return bsp_list
+
